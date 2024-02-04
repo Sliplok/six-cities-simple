631c1df9c6c06e1821e056eaa01e6306d1ac9eb7
Владимир <vovavasin2@mail.ru>
2024-02-04 11:54:19 +0700

    Добавление маршрутизации

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,8 @@
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFound from '../../pages/not-found-page/not-found-page';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +10,14 @@ type AppProps = {
 
 function App({placesCount} : AppProps) : JSX.Element {
   return (
-    <MainPage placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage placesCount={placesCount}/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/offer' element={<OfferPage/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,7 @@
+function NotFound() : JSX.Element {
+  return (
+    <h1>Ошибка 404. Страница не существует.</h1>
+  );
+}
+
+export default NotFound;
